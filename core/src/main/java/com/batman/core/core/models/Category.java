c92e0cb4a50a7dc3c96cc81df23fb47cdd7d2246
Arpit Gupta <arpit.gupta@diconium.com>
2020-07-07 16:30:13 +0200

    feat: Updated Category Component with Model Exporter

--- core/src/main/java/com/batman/core/core/models/Category.java
+++ core/src/main/java/com/batman/core/core/models/Category.java
@@ -2,10 +2,14 @@ package com.batman.core.core.models;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-@Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Model(adaptables = SlingHttpServletRequest.class,
+    resourceType = "batman/components/content/category",
+    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = "jackson", extensions = "json")
 public class Category {
 
   @ValueMapValue
